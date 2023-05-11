3ef4a4c9d08ba4a06fe2006dedcb2f816d1f3fc9
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-05-11 20:47:23 +1000

    Update tariffs_time_series.r

--- tariffs_time_series.r
+++ tariffs_time_series.r
@@ -19,7 +19,6 @@ product_list <- list("940370") #Can do as a list e.g. list("100199","100190")
 #What years do you want?
 years_list <- as.list(1988:2020)
 
-
 #Insert codes that are considered to be the same product, e.g. wheat used to be 100190, and is now effectively 100199.
 common_commodity_code <- function() {
 	#No concordance needed for plastic furniture (HS codes haven't changed, but wheat example is given below)
