c928360800f7a8c4a076268ec86169ebaad0cd27
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-05-11 20:47:02 +1000

    Update tariffs_time_series.r

--- tariffs_time_series.r
+++ tariffs_time_series.r
@@ -13,17 +13,18 @@ library(dplyr)
 RTA_groupings_CSV <- 'pref_imput_groups.csv' #This is a custom file created by me, interpreting inconsistent RTA descriptions and grouping them as consistent RTAs. The basis for this is the file at http://wits.worldbank.org/data/public/TRAINSPreferenceBenficiaries.xls
 RTA_members_CSV <- 'RTA_members.csv' #Create this file yourself from http://wits.worldbank.org/data/public/TRAINSPreferenceBenficiaries.xls
 
-#Insert codes that are considered to be the same product, e.g. wheat used to be 100190, and is now effectively 100199.
-common_commodity_code <- function() {
-	tariffs_df$PRODUCTCODE[tariffs_df$PRODUCTCODE == "100190"] <- "100199"
-}
 
 #1. What codes do you want?
-product_list <- list("100199","100190")
+product_list <- list("940370") #Can do as a list e.g. list("100199","100190")
 #What years do you want?
 years_list <- as.list(1988:2020)
 
 
+#Insert codes that are considered to be the same product, e.g. wheat used to be 100190, and is now effectively 100199.
+common_commodity_code <- function() {
+	#No concordance needed for plastic furniture (HS codes haven't changed, but wheat example is given below)
+	#tariffs_df$PRODUCTCODE[tariffs_df$PRODUCTCODE == "100190"] <- "100199"
+}
 
 
 
