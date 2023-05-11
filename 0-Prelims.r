81f37a359bc552166c59daa79d410e003fb72783
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-05-11 20:47:39 +1000

    Update 0-Prelims.r

--- 0-Prelims.r
+++ 0-Prelims.r
@@ -12,8 +12,3 @@ library(dplyr)
 #Insert file names
 RTA_groupings_CSV <- 'pref_imput_groups.csv' #This is a custom file created by me, interpreting inconsistent RTA descriptions and grouping them as consistent RTAs. The basis for this is the file at http://wits.worldbank.org/data/public/TRAINSPreferenceBenficiaries.xls
 RTA_members_CSV <- 'RTA_members.csv' #Create this file yourself from http://wits.worldbank.org/data/public/TRAINSPreferenceBenficiaries.xls
-
-#Insert codes that are considered to be the same product, e.g. wheat used to be 100190, and is now effectively 100199.
-common_commodity_code <- function() {
-	tariffs_df$PRODUCTCODE[tariffs_df$PRODUCTCODE == "100190"] <- "100199"
-}
