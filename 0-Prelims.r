9a1b164e68115a015808b61e53a008f5cc9fe3d5
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-05-14 15:27:37 +1000

    Update 0-Prelims.r

--- 0-Prelims.r
+++ 0-Prelims.r
@@ -10,5 +10,7 @@ library(tidyr)
 library(dplyr)
 
 #Insert file names
+#Download the following csv file from https://github.com/jamesfell0000/build_tariff_time_series/blob/main/pref_imput_groups.csv
 RTA_groupings_CSV <- 'pref_imput_groups.csv' #This is a custom file created by me, interpreting inconsistent RTA descriptions and grouping them as consistent RTAs. The basis for this is the file at http://wits.worldbank.org/data/public/TRAINSPreferenceBenficiaries.xls
+#Download the following csv file from https://github.com/jamesfell0000/build_tariff_time_series/blob/main/RTA_members.csv
 RTA_members_CSV <- 'RTA_members.csv' #Create this file yourself from http://wits.worldbank.org/data/public/TRAINSPreferenceBenficiaries.xls
